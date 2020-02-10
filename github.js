43c54b6cc6c5262fc0f5381226f7a0b2174a6f2c
prayans <tyagipriyanshu077@gmail.com>
2020-02-10 21:47:57 +0530

    my first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'dc07346371d175dc88d5';
+    this.client_secret = 'b645e3cba351bf8d11afd897d00334ede32fc89e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+   
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
